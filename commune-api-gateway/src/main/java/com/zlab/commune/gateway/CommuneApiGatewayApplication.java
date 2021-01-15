9b9a929ee85d052a1fc99c9d8d539b630d2b8bb7
Zubair <zubair@PagaAdmins-BabsEngineering.local>
2021-01-15 20:52:47 +0100

    initial setup for the api gateway

---
+++ commune-api-gateway/src/main/java/com/zlab/commune/gateway/CommuneApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zlab.commune.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuneApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuneApiGatewayApplication.class, args);
+	}
+
+}
